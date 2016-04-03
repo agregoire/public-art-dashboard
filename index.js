4085827d79d1b9dfa09343935d9e218a2c67660e
Alexandre Grégoire <alexandre.gregoire@gmail.com>
2016-04-03 16:10:56 -0400

    Collect results in an array

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+'use strict';
+
 const async = require('async');
 const fs = require('fs');
 const fetcher = require('./lib/fetcher');
 const cities = JSON.parse(fs.readFileSync('./cities.json').toString());
+let results = [];
 
 async.each(cities, (city, callback) => {
-  fetcher.fetch(city.dataFormat, city.feedUrl, city.name, city.key, callback);
+  fetcher.fetch(city, (err, count) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Got ${count} for city ${city.name}`);
+    results.push([city, count]);
+    callback(err);
+  });
+}, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(results);
 });
